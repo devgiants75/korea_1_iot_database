b2507c4a136dcd921179793b07f815b85fc19017
devgiants75 <devgiants75@naver.com>
2024-09-24 12:41:05 +0900

    20240923

--- d_dml/b_제약조건.sql
+++ d_dml/b_제약조건.sql
@@ -72,6 +72,8 @@ value
 alter table `Students`
 drop primary key;
 
+# cf) 기본 키 제약 조건 삭제 시 not null에 대한 조건은 사라지지 않음
+
 desc `Students`;
 
 # 2) 제약조건 추가
@@ -79,3 +81,86 @@ alter table `Students`
 add primary key (student_id);
 
 desc `Students`;
+
+/*
+	2. FK, Foreign Key (외래 키)
+	: 두 테이블 사이의 관계를 연결, 데이터의 무결성을 유지
+    : 외래 키가 설정된 열은 반드시 다른 테이블의 기본 키와 연결!
+*/
+
+# 기본 테이블: 기본 키가 있는 테이블 (회원 테이블)
+# 참조 테이블: 외래 키가 있는 테이블 (구매 테이블)
+
+# 외래 키 사용 예시
+# : 회원 (Members) - 주문 (Orders)
+# >> 고객이 실제로 존재하는 지 확인, 고객과 주문 간의 관계를 명시
+
+create table `members` (
+	member_id int primary key,
+    name varchar(100)
+);
+
+create table `orders` (
+	order_id int primary key,
+    order_date date,
+    member_id int,
+    # 외래 키 지정 방식
+    # foreign key (참조컬럼) references 기본테이블(기본컬럼)
+    FOREIGN KEY (member_id) references `members`(member_id)
+);
+
+-- 유효하지 않은 고객 참조 시도 (오류)
+-- INSERT INTO `orders`
+-- values (1, '2024-09-24', 123); # Error: 참조 무결성 위반
+
+-- 고객 데이터 입력
+insert into `members`
+value (1, '황상기');
+
+-- 유효한 고객 참조 시도
+insert into `orders`
+value (1, '2024-09-23', 1);
+
+# 외래 키 제약조건 삭제 & 추가
+# : 외래 키 제약조건 삭제 시 주의점
+# >> 해당 데이터를 참조하는 데이터가 있을 경우 삭제할 수 없음
+
+# 외래 키 제약 조건 이름 확인
+select constraint_name
+from information_schema.key_column_usage
+where table_name = 'orders' and column_name = 'member_id';
+
+# >> orders_ibfk_1
+
+alter table `orders`
+drop FOREIGN KEY `orders_ibfk_1`;
+# >> 외래 키 제약조건 삭제 시 제약조건의 이름을 검색하여 작성
+
+# 외래 키 제약조건 재추가
+alter table `orders`
+add constraint
+	FOREIGN KEY (member_id)
+    REFERENCES members(member_id);
+
+DESCRIBE `orders`;
+DESCRIBE `members`;
+
+/*
+	3. UNIQUE
+    : 특정 열에 대해 모든 값이 고유해야 함을 보장
+    : 한 테이블 내에서 여러 개 지정 가능 (각각 다른 컬럼에 독립적으로 적용)
+    : Null값 허용
+	
+    cf) PK와의 차이
+    >> 여러 컬럼에 사용 가능 & NULL 값 허용
+*/
+create table `users` (
+	user_id int primary key,
+    user_name varchar(100),
+    user_email varchar(100) unique
+);
+
+insert into `users`
+values
+	(1, '박영준', 'qwe123'),
+	(2, '박영준', 'qwe124');
